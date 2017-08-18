03b90eeda812171aabfaf92382c71897c124310e
james <james@gmail.com>
2017-08-18 10:14:03 +0800

    a

---
+++ PhippyTenant/Home/ShopActivityViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShopActivityViewController.h
+//  PhippyTenant
+//
+//  Created by toby on 18/08/2017.
+//  Copyright © 2017 kg.self.edu. All rights reserved.
+//
+
+#import "PhippyViewController.h"
+
+@interface ShopActivityViewController : PhippyViewController
+
+@end
---
+++ PhippyTenant/Home/ShopActivityViewController.m
@@ -0,0 +1,37 @@
+//
+//  ShopActivityViewController.m
+//  PhippyTenant
+//
+//  Created by toby on 18/08/2017.
+//  Copyright © 2017 kg.self.edu. All rights reserved.
+//
+
+#import "ShopActivityViewController.h"
+
+@interface ShopActivityViewController ()
+
+@end
+
+@implementation ShopActivityViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
